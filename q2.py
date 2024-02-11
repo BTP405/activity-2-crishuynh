86c171011e9dbc32d6a9304cc5577c9da4ba64af
cris-huynh <158625306+crishuynh@users.noreply.github.com>
2024-02-11 13:16:08 -0500

    push

--- q2.py
+++ q2.py
@@ -1,1 +1,27 @@
-def graphSnowfall(t):
+import matplotlib.pyplot as plt
+
+def graphSnowFall(t): 
+    with open(t, 'r') as f:
+        ranges = f.readlines()
+        for each in range(len(ranges)): 
+            ranges[each] = int(ranges[each])
+            
+        range_0_10 = range_11_20 = range_21_30 = range_31_40 = range_41_50 = 0
+
+        for i in range(len(ranges)):
+            if ranges[i] <= 10: range_0_10 += 1
+            elif ranges[i] >= 11 and ranges[i] <= 20:  range_11_20 += 1
+            elif ranges[i] >=21 and ranges[i] <= 30:  range_21_30 += 1
+            elif ranges[i] >= 31 and ranges[i] <= 40:  range_41_50 += 1
+            else: range_41_50 += 1
+
+        x = ['0 - 10', '11 - 20', '21 - 30', '31 - 40', '41 - 50']
+        y = [range_0_10, range_11_20, range_21_30, range_31_40, range_41_50]; 
+        plt.bar(x, y)
+        plt.title('Snowfall')
+        plt.xlabel('Range (cm)')
+        plt.ylabel('Amount')
+        plt.yticks([0, 1, 2, 3])
+        plt.show() 
+
+graphSnowFall('q2.txt')
--- q4.py
+++ q4.py
@@ -1,1 +1,32 @@
+def decoratorFunc(func):
+    def wrapper(line):
+        func(line)
+        nums = line.split()
+        for i in range(0, len(nums)):
+            nums[i] = int(nums[i])
+        print("The numbers read: ", nums)
+        print("The count of the numbers read: ", len(nums))
+        print("The average of the numbers: ", sum(nums) / len(nums))
+        print("The maximum of the numbers: ", max(nums))
+        return func(nums)
+    return wrapper
+
+@decoratorFunc
+def process_line(line):
+    print("--------")
+
 def printStats(t):
+    with open(t, 'r') as f:
+        lines = f.readlines()
+        for i in range(0, len(lines)):
+            print("Line: " + str(i + 1))
+            process_line(lines[i])
+
+def main():
+    printStats("q4.txt")
+
+
+if __name__ == "__main__":
+    main()
+
+
